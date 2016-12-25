01fd747a8158c7decd3c036627f9d21e8774ccb5
yuzhucu'git config --global user.name yuzhucugit config --global user.name "yuzhucu <yuzhucu@126.com>
2016-12-25 17:36:23 +0800

    java

--- src/com/cssm/common/pojo/User.java
+++ src/com/cssm/common/pojo/User.java
@@ -2,12 +2,24 @@ package com.cssm.common.pojo ;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private int userId;
+	
+	@NotEmpty(message = "用户名不能为空")
 	private String userName;
+	
+	@Length(min=6 ,max= 20 ,message = "密码长度不符合标准")
 	private String passWord;
+	
+	@Email(message = "邮箱格式不对")
+	private String email;
+	
 	private int gender;
 
 	public int getUserId() {
@@ -41,4 +53,33 @@ public class User implements Serializable {
 	public void setGender(int gender) {
 		this.gender = gender;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", passWord=" + passWord + ", email=" + email
+				+ ", gender=" + gender + "]";
+	}
+
+	public User(int userId, String userName, String passWord, String email, int gender) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.passWord = passWord;
+		this.email = email;
+		this.gender = gender;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
